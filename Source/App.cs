f85a0e2e32cd0793da861b2da0a9af32ea4d79f1
Deivids Dukalskis <deivids.dukalskis@accenture.com>
2022-04-07 13:20:24 +0300

    1. Added Game class to the project 2. Added main game menu to select field size for game 3. Added 1 waiting thread to the playing state to check if ESC is not pressed. If it is then exit to menu. 4. Added 1s refresh to the console with s letter.

---
+++ Source/App.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Source
+{
+    public class App
+    {
+        private Game? game;
+        private bool shouldRun = false;
+        public App()
+        {
+            shouldRun = true;
+            game = new Game();
+        }
+        public void Run()
+        {
+            while (shouldRun)
+            {
+                Console.SetWindowSize(120, 30);
+                Console.Clear();
+                Console.WriteLine("1. 40x35 game");
+                Console.WriteLine("2. 60x35 game");
+                Console.WriteLine("3. 80x35 game");
+                Console.WriteLine("ESC to exit");
+                ConsoleKeyInfo selection = Console.ReadKey();
+                Console.WriteLine();
+                switch (selection.Key)
+                {
+                    case ConsoleKey.D1:
+                        game.Play(40, 35);
+                        break;
+                    case ConsoleKey.D2:
+                        game.Play(60, 35);
+                        break;
+                    case ConsoleKey.D3:
+                        game.Play(80, 35);
+                        break;
+                    case ConsoleKey.Escape:
+                        shouldRun = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+        }
+        public static void Main()
+        {
+            App app = new App();
+            app.Run();
+        }
+    }
+}
---
+++ Source/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife.Source
+{
+    public class Game
+    {
+        public bool shouldRun = false;
+        public void Play(ushort fieldWidth, ushort fieldHeight)
+        {
+            Console.SetWindowSize(fieldWidth * 2, fieldHeight);
+            Console.Clear();
+            shouldRun = true;
+            Thread checkIfShouldRun = new Thread(CheckIfShouldRun);
+            checkIfShouldRun.Start();
+            while (shouldRun)
+            {
+                Console.Write("s");
+                Thread.Sleep(1000);
+            }
+            checkIfShouldRun.Join();
+        }
+        public void CheckIfShouldRun()
+        {
+            while(Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                shouldRun = true;
+            }
+            shouldRun = false;
+        }
+    }
+}
